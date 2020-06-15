1bedd2ddd1f94493e9bb139c32ad1724749efe85
Miguel36 <miguelmontilla.1936@gmail.com>
2020-06-15 00:55:14 -0500

    Configurando la navegacion en el actionBar con la back arrow

--- app/src/main/java/com/mmontilla/dogs/view/MainActivity.kt
+++ app/src/main/java/com/mmontilla/dogs/view/MainActivity.kt
@@ -2,12 +2,31 @@ package com.mmontilla.dogs.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.mmontilla.dogs.R
-import kotlinx.android.synthetic.main.fragment_list.*
 
 class MainActivity : AppCompatActivity() {
+
+     lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * the title in the action bar will automatically be updated when
+         * the destination changes according to the label in navGraph
+         */
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    /**
+     * Allow the navigation in actionbar using backarrow
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
