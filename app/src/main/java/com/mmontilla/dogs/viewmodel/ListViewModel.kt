47632ccf5e45930b97138c943524c6eee2a84e27
Miguel36 <miguelmontilla.1936@gmail.com>
2020-06-22 17:29:39 -0500

    Creando el viewmodel para ListFragment

---
+++ app/src/main/java/com/mmontilla/dogs/viewmodel/ListViewModel.kt
@@ -0,0 +1,23 @@
+package com.mmontilla.dogs.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.mmontilla.dogs.model.DogBreed
+
+class ListViewModel: ViewModel() {
+
+    val dogs = MutableLiveData<List<DogBreed>>()
+    val dogsLoadError = MutableLiveData<Boolean>()
+    val loading =MutableLiveData<Boolean>()
+
+    fun refresh() {
+        val dog1 = DogBreed("1", "Corgi", "15 years", "breedGroup", "breedFor", "temperament", "")
+        val dog2 = DogBreed("2", "labrador", "10 years", "breedGroup", "breedFor", "temperament", "")
+        val dog3 = DogBreed("3", "Rotwailer", "20 years", "breedGroup", "breedFor", "temperament", "")
+        val dogList = arrayListOf<DogBreed>(dog1, dog2, dog3)
+
+        dogs.value = dogList
+        dogsLoadError.value = false
+        loading.value = false
+    }
+}
